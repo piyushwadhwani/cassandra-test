21881d94c5bd8024f44ba59262c76fec830314ce
Piyush Wadhwani <piyush.wadhwani@gmail.com>
2020-05-14 15:55:37 -0400

    added test container

---
+++ src/test/java/com/tekdynamix/cassandra/CassandraDBResource.java
@@ -0,0 +1,20 @@
+package com.tekdynamix.cassandra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.CassandraContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public class CassandraDBResource {
+
+    private static final Logger log = LoggerFactory.getLogger(CassandraDBResource.class);
+
+
+
+    //@Test
+    //void someTestMethod() {
+
+    //}
+
+
+}
--- src/test/java/com/tekdynamix/cassandra/CassandraResourceTest.java
+++ src/test/java/com/tekdynamix/cassandra/CassandraResourceTest.java
@@ -2,14 +2,25 @@ package com.tekdynamix.cassandra;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.CassandraContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class CassandraResourceTest {
+    private static final Logger log = LoggerFactory.getLogger(CassandraDBResource.class);
+
+
+    @Container
+    private static final CassandraContainer cassandraContainer = new CassandraContainer();
+
     @Test
     public void testHelloEndpoint() {
+        log.info("Starting Testing for Rest Endpoing test");
         given()
           .when().get("/test")
           .then()
