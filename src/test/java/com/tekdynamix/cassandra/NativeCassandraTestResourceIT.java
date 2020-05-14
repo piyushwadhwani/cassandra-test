be9f8799d20d28b18b7f232f7ac4bfe95bf4da30
Piyush Wadhwani <piyush.wadhwani@equifax.com>
2020-05-14 15:13:15 -0400

    Initial Commit

---
+++ src/test/java/com/tekdynamix/cassandra/NativeCassandraTestResourceIT.java
@@ -0,0 +1,9 @@
+package com.tekdynamix.cassandra;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCassandraTestResourceIT extends CassandraTestResource {
+
+    // Execute the same tests but in native mode.
+}
