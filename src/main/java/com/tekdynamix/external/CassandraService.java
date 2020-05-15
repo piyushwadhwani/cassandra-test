4b150d42582e55c5a5344980f23dff69909595d2
Piyush Wadhwani <piyush.wadhwani@equifax.com>
2020-05-14 21:08:55 -0400

    cassandra java driver datastax driver fix

--- src/main/java/com/tekdynamix/external/CassandraService.java
+++ src/main/java/com/tekdynamix/external/CassandraService.java
@@ -1,8 +1,6 @@
 package com.tekdynamix.external;
 
-import com.datastax.driver.core.Row;
-import io.vertx.cassandra.CassandraClient;
-import io.vertx.cassandra.CassandraClientOptions;
+import com.datastax.oss.driver.api.core.CqlSession;
 import io.vertx.core.Vertx;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
@@ -11,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.List;
 
 @ApplicationScoped
 public class CassandraService {
@@ -36,23 +33,30 @@ public class CassandraService {
     private static final Logger log = LoggerFactory.getLogger(CassandraService.class);
 
 
-    CassandraClient client;
+    @Inject
+    CqlSession session;
+
+
+    //CassandraClient client;
 
     @PostConstruct
     void init() {
         log.info("Initializing Cassandra Client");
-        CassandraClientOptions options = new CassandraClientOptions()
-                .addContactPoint(dbHost).setPort(Integer.parseInt(dbPort));
-        client = CassandraClient.createShared(vertx, "sharedClientName", options);
-        log.info(" Cassandra Client Connection Status {}", client.isConnected());
-        
+
+        //CassandraClientOptions options = new CassandraClientOptions()
+        //      .addContactPoint(dbHost).setPort(Integer.parseInt(dbPort));
+        //client = CassandraClient.createShared(vertx, "sharedClientName", options);
+        log.info(" Cassandra Client Connection Status {}", session.getMetadata());
+
 
     }
 
 
     public String getResource() {
 
-        client.executeWithFullFetch("SELECT * FROM "+keysapce+"."+table+"  ", executeWithFullFetch -> {
+        session.executeReactive("SELECT * FROM " + keysapce + "." + table);
+
+       /* client.executeWithFullFetch("SELECT * FROM "+keysapce+"."+table+"  ", executeWithFullFetch -> {
             if (executeWithFullFetch.succeeded()) {
                 List<Row> rows = executeWithFullFetch.result();
                 for (Row row : rows) {
@@ -62,7 +66,7 @@ public class CassandraService {
                 log.error("Unable to execute the query",executeWithFullFetch.cause());
                 executeWithFullFetch.cause().printStackTrace();
             }
-        });
+        });*/
 
         return "hello";
 
